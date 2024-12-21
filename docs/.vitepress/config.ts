118287fc068410bd71c9f019f5799e49126298b4
青稚 <sweetcandymini@foxmail.com>
2024-12-21 09:36:23 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+head: [
+      ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ],
  // cleanUrls: true,
   base: "/",
   markdown: {
